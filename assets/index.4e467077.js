8c07637a60a1aa16ed20b97c9ed88abc7fcfc0e7
v_ykaiyu <v_ykaiyu@tencent.com>
2022-09-30 17:34:20 +0800

    完成组件开发

---
+++ assets/index.4e467077.js
@@ -0,0 +1,1 @@
+import{d as a,r as c,o as s,b as t}from"./index.b3f7048f.js";const p=a({__name:"index",setup(r){const e=o=>{console.log(o)};return(o,_)=>{const n=c("m-choose-area");return s(),t(n,{onChange:e})}}});export{p as default};
