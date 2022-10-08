826d8d1495a5c4eeb681d0a355402889f876d147
v_ykaiyu <v_ykaiyu@tencent.com>
2022-10-08 16:19:46 +0800

    修改

--- assets/index.cda0b9f0.js
+++
@@ -1,1 +0,0 @@
-import{_ as r,r as n,o as s,c as a,f as t,F as c}from"./index.18c489b9.js";const o={};function i(p,m){const e=n("m-progress");return s(),a(c,null,[t(e,{percentage:60,isAnimate:""}),t(e,{"text-inside":!0,"stroke-width":26,percentage:70,isAnimate:""}),t(e,{type:"circle",status:"exception",percentage:60,isAnimate:""})],64)}var l=r(o,[["render",i]]);export{l as default};
